79897db4e9e213f88baaa96496add9bcb4e29bc1
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2022-12-19 18:46:21 +1300

    ⚗️ Click event experiment (need to be updated)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,31 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
+import { useCallback } from 'react';
+
 
 export default function Home() {
+  const foo = 1
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.innerText)
+    e.preventDefault();
+    alert(foo);
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index page</title>
       </Head>
       <Header />
-      <Main page="index" />
+      <a
+        href='/about'
+        onClick={handleClick}
+      >
+        Button
+      </a>
+      <Main page='index' />
       <Footer />
     </div>
   );
