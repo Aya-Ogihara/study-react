1fe5630acf8db5841eecb753585b8da246209e9e
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2022-12-26 11:07:43 +1300

    ✨ Use useRouter

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,19 +2,19 @@ import 'src/styles/globals.css';
 import Head from 'next/head';
 import { useCounter } from 'src/hooks/useCounter';
 import { useInputArray } from 'src/hooks/useInputArray';
-import { useBgLightblue } from 'src/hooks/useBgLightblue';
+import { useBgColor } from 'src/hooks/useBgColor';
 
 function MyApp({ Component, pageProps }) {
   const counter = useCounter();
   const inputArray = useInputArray();
-  useBgLightblue();
+  useBgColor();
 
   return (
     <>
       <Head>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArray} useBgLightblue />
+      <Component {...pageProps} {...counter} {...inputArray} useBgColor />
     </>
   );
 }
