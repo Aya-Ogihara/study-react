7a9c71abfb32f29fc0d20564aa70d3719eedb35a
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2022-12-25 15:58:34 +1300

    💄 Change UI

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,22 +19,22 @@ export default function About() {
         <title>About page</title>
       </Head>
       <Header />
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleDisplay}>
         {isShow ? 'Hide count' : 'Display count'}
       </button>
       <br />
-      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleAddClick}>+</button>
       <button onClick={handleDeductClick}>-</button>
       <br />
       <input type='text' value={text} onChange={handleChange} />
       <br />
       <button onClick={handleAddItem}>Add item</button>
-      <ul>
+      <ol>
         {array.map((item) => {
           return <li key={item}>{item}</li>;
         })}
-      </ul>
+      </ol>
       <Main page="about" />
 
       <Footer />
