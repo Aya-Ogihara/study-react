47af7df5ccb0cbc7d9cd272cdc4e4648dd2f470a
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2022-12-25 16:17:08 +1300

    ✨ Use State lift up among pages

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,15 +3,19 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useCounter } from 'src/hooks/useCounter';
-import { useInputArray } from 'src/hooks/useInputArray';
-import { useBgLightblue } from 'src/hooks/useBgLightblue';
 
-
-export default function About() {
-  const {count, isShow, handleAddClick, handleDeductClick, handleDisplay} = useCounter();
-  const {text, array, handleChange, handleAddItem} = useInputArray();
-  useBgLightblue();
+export default function About(props) {
+  const {
+    count,
+    isShow,
+    handleAddClick,
+    handleDeductClick,
+    handleDisplay,
+    text,
+    array,
+    handleChange,
+    handleAddItem,
+  } = props;
 
   return (
     <div className={styles.container}>
@@ -35,7 +39,7 @@ export default function About() {
           return <li key={item}>{item}</li>;
         })}
       </ol>
-      <Main page="about" />
+      <Main page='about' />
 
       <Footer />
     </div>
