eb3480bc62e9a7c6dd72c25faa07ea82c70960fa
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2022-12-26 11:29:49 +1300

    ♻️ Refactoring

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -28,7 +28,7 @@ const ITEMS = [
   },
 ];
 
-export function Main(props) {
+export const Main = (props) => {
   const [items, setItems] = useState(ITEMS);
   const reduceItem = useCallback(() => {
     setItems((prevItems) => {
